b1c4578c2a4d0e24d601bb5bd3cfc5333abbbd6b
devjohnpark <devjohnpark@gmail.com>
2026-06-11 19:47:05 +0900

    [BOJ] 15650 15652 N개의 수 중에서 M개의 조합 찾기

---
+++ coding-test/src/combination/BJ_15650_N개의수중에서M개조합찾기_중복허용.java
@@ -0,0 +1,39 @@
+package combination;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_15650_N개의수중에서M개조합찾기_중복허용 {
+    static int N;
+    static int M;
+    static int[] mark;
+
+    // 1 ~ N개의 수중에 M개 조합
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        mark = new int[N];
+        combination(1, 0);
+    }
+
+    private static void combination(int start, int cnt) {
+        if (cnt >= M) {
+            // 2개 조합
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < M; i++) {
+                sb.append(mark[i]).append(" ");
+            }
+            System.out.println(sb.toString());
+            return;
+        }
+
+        for (int i=start; i<=N; i++) {
+            mark[cnt] = i;
+            combination(i, cnt+1);
+        }
+    }
+}
---
+++ coding-test/src/combination/BJ_3035_9개중에서조건에맞는7개찾기.java
@@ -0,0 +1,7 @@
+package combination;
+
+public class BJ_3035_9개중에서조건에맞는7개찾기 {
+    public static void main(String[] args) {
+
+    }
+}
